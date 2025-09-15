8c0a6a4f5b2812ce63c7b82d851167ee213a7fc1
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-09-15 12:45:26 -0300

    Exibição dos games utilizando um Card

--- app/src/main/java/carreiras/com/github/fundamentos_jetpack_compose_listas_lazy/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/fundamentos_jetpack_compose_listas_lazy/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,16 +16,21 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import carreiras.com.github.fundamentos_jetpack_compose_listas_lazy.components.GameCard
+import carreiras.com.github.fundamentos_jetpack_compose_listas_lazy.model.Game
 import carreiras.com.github.fundamentos_jetpack_compose_listas_lazy.repository.getAllGames
 import carreiras.com.github.fundamentos_jetpack_compose_listas_lazy.ui.theme.FundamentosjetpackcomposelistaslazyTheme
 
@@ -69,9 +76,7 @@ fun GamesScreen(modifier: Modifier = Modifier) {
         Spacer(modifier = Modifier.height(16.dp))
         LazyColumn() {
             items(getAllGames()) {
-                Column() {
-                    Text(text = it.title)
-                }
+                GameCard(game = it)
             }
         }
     }
